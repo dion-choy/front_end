6fdfe91ccbea78e5ad6c72e12478dc3dfdad359e
dion-choy <dionchoynj@gmail.com>
2024-09-07 01:42:07 +0800

    created cancel button and functionality

--- to-do/script.js
+++ to-do/script.js
@@ -3,19 +3,10 @@ function addToList(event) {
     const newItem = document.createElement("li");
     const checkBox = document.createElement("input");
     const newToDo = document.getElementById("task").value.trim();
+    const removeButton = document.createElement("button");
 
-    checkBox.type = "checkBox";
-    checkBox.addEventListener("click", () => {
-        if (newItem.classList[0] == "undone") {
-            newItem.className = "done";
-        } else if (newItem.classList[0] == "done") {
-            newItem.className = "undone";
-        }
-        localStorage.setItem(newItem.dataset.index, [newItem.classList[0], newToDo]);
-    });
+    addButtonFunc(checkBox, newItem, newToDo, removeButton);
 
-    newItem.append(checkBox);
-    newItem.append(newToDo);
     newItem.className = "undone";
     newItem.dataset.index = ++taskNum;
 
@@ -23,41 +14,56 @@ function addToList(event) {
 
     document.getElementById("task").value = "";
 
-    localStorage.setItem(taskNum, [newItem.classList[0], newToDo]);
+    localStorage.setItem(taskNum, [newItem.className, newToDo]);
     localStorage.setItem("size", taskNum);
+}
+
+function addButtonFunc(checkBox, newItem, newToDo, removeButton) {
+    newItem.append(checkBox, newToDo, removeButton);
+
+    removeButton.innerText = "✖";
+    removeButton.className = "removeTask";
+    removeButton.addEventListener("click", clearTask);
+
+    checkBox.type = "checkBox";
+    checkBox.addEventListener("click", () => {
+        newItem.className = newItem.className == "undone" ? "done" : "undone";
+        localStorage.setItem(newItem.dataset.index, [newItem.className, newToDo]);
+    });
+}
+
+function clearTask() {
+    const removedTask = this.parentElement;
+    localStorage.removeItem(removedTask.dataset.index);
+    toDo.removeChild(this.parentElement);
+
+    const tasks = document.getElementsByTagName("li");
+    localStorage.clear();
+    localStorage.setItem("size", --taskNum);
+
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i].dataset.index = i + 1;
+        localStorage.setItem(i + 1, [tasks[i].className, tasks[i].childNodes[1].textContent]);
+    }
 }
 
 window.onload = function () {
     if (localStorage.size) {
         taskNum = localStorage.size;
-        alert(`${localStorage.size} loading...`);
-        for (let i = 1; i <= localStorage.size; i++) {
+        for (let i = 1; i <= taskNum; i++) {
             task = localStorage.getItem(i).split(",");
 
             const newItem = document.createElement("li");
             const checkBox = document.createElement("input");
             const newToDo = task[1];
+            const removeButton = document.createElement("button");
 
             checkBox.type = "checkBox";
-            if (task[0] == "done") {
-                checkBox.checked = true;
-                newItem.className = "done";
-            } else if (task[0] == "undone") {
-                checkBox.checked = false;
-                newItem.className = "undone";
-            }
+            newItem.className = task[0];
+            checkBox.checked = task[0] == "done";
 
-            checkBox.addEventListener("click", () => {
-                if (newItem.classList[0] == "undone") {
-                    newItem.className = "done";
-                } else if (newItem.classList[0] == "done") {
-                    newItem.className = "undone";
-                }
-                localStorage.setItem(newItem.dataset.index, [newItem.classList[0], newToDo]);
-            });
+            addButtonFunc(checkBox, newItem, newToDo, removeButton);
 
-            newItem.append(checkBox);
-            newItem.append(newToDo);
             newItem.dataset.index = i;
 
             toDo.appendChild(newItem);
