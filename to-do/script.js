ee897e0510c9d980d30922c97d20bf6f5bd27372
dion-choy <dionchoynj@gmail.com>
2024-09-07 22:44:45 +0800

    improved stylesheet to center text and format list with wrapping text

--- to-do/script.js
+++ to-do/script.js
@@ -2,24 +2,28 @@ function addToList(event) {
     event.preventDefault();
     const newItem = document.createElement("li");
     const checkBox = document.createElement("input");
-    const newToDo = document.getElementById("task").value.trim();
+    const newTask = document.createElement("p");
     const removeButton = document.createElement("button");
+    const lineBreak = document.createElement("hr");
 
-    addButtonFunc(checkBox, newItem, newToDo, removeButton);
+    newTask.innerText = document.getElementById("task").value.trim();
 
-    newItem.className = "undone";
+    addButtonFunc(checkBox, newItem, newTask, removeButton);
+    newItem.appendChild(lineBreak);
+
+    newTask.className = "undone";
     newItem.dataset.index = ++taskNum;
 
     toDo.appendChild(newItem);
 
     document.getElementById("task").value = "";
 
-    localStorage.setItem(taskNum, [newItem.className, newToDo]);
+    localStorage.setItem(taskNum, [newTask.className, newTask.innerText]);
     localStorage.setItem("size", taskNum);
 }
 
-function addButtonFunc(checkBox, newItem, newToDo, removeButton) {
-    newItem.append(checkBox, newToDo, removeButton);
+function addButtonFunc(checkBox, newItem, task, removeButton) {
+    newItem.append(checkBox, removeButton, task);
 
     removeButton.innerText = "✖";
     removeButton.className = "removeTask";
@@ -27,24 +31,25 @@ function addButtonFunc(checkBox, newItem, newToDo, removeButton) {
 
     checkBox.type = "checkBox";
     checkBox.addEventListener("click", () => {
-        newItem.className = newItem.className == "undone" ? "done" : "undone";
-        localStorage.setItem(newItem.dataset.index, [newItem.className, newToDo]);
+        task.className = task.className == "undone" ? "done" : "undone";
+        localStorage.setItem(newItem.dataset.index, [task.className, task.innerText]);
     });
 }
 
 function clearTask() {
     const removedTask = this.parentElement;
+    const oldIndex = removedTask.dataset.index;
     localStorage.removeItem(removedTask.dataset.index);
     toDo.removeChild(this.parentElement);
 
     const tasks = document.getElementsByTagName("li");
-    localStorage.clear();
     localStorage.setItem("size", --taskNum);
 
-    for (let i = 0; i < tasks.length; i++) {
+    for (let i = oldIndex - 1; i < tasks.length; i++) {
         tasks[i].dataset.index = i + 1;
-        localStorage.setItem(i + 1, [tasks[i].className, tasks[i].childNodes[1].textContent]);
+        localStorage.setItem(i + 1, [tasks[i].childNodes[2].className, tasks[i].childNodes[2].textContent]);
     }
+    localStorage.removeItem(taskNum + 1);
 }
 
 window.onload = function () {
@@ -55,14 +60,17 @@ window.onload = function () {
 
             const newItem = document.createElement("li");
             const checkBox = document.createElement("input");
-            const newToDo = task[1];
+            const loadedTask = document.createElement("p");
+            loadedTask.innerText = task[1];
             const removeButton = document.createElement("button");
+            const lineBreak = document.createElement("hr");
 
             checkBox.type = "checkBox";
-            newItem.className = task[0];
+            loadedTask.className = task[0];
             checkBox.checked = task[0] == "done";
 
-            addButtonFunc(checkBox, newItem, newToDo, removeButton);
+            addButtonFunc(checkBox, newItem, loadedTask, removeButton);
+            newItem.appendChild(lineBreak);
 
             newItem.dataset.index = i;
 
